264aa6bcf8344390b065c615be6aafbece7ac71a
phuongvo1110 <vptphuong21@clc.fitus.edu.vn>
2024-11-03 17:56:20 +0700

    adding file

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard';
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path='' element={<Dashboard/>}/>
+                {/* Add more routes for Home, Login, etc. */}
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+const Login = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const [errorMessage, setErrorMessage] = useState("");
+    const navigate = useNavigate();
+    const onSubmit = async (data) => {
+        try {
+            const response = await axios.post(
+                "https://localhost:7242/user/login",
+                data
+            );
+            alert("Login successful!");
+            navigate("/");
+        } catch (error) {
+            if (error) {
+                setErrorMessage(error.response.data);
+                console.log(errorMessage);
+            } else {
+                alert("An error occurred. Please try again.");
+            }
+        }
+    };
+
+    return (
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Login
+                        </h1>
+                        <form
+                            className="space-y-4 md:space-y-6"
+                            onSubmit={handleSubmit(onSubmit)}
+                        >
+                            <div>
+                                <label
+                                    htmlFor="email"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Your email
+                                </label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    {...register("email", {
+                                        required: "Email is required",
+                                    })}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="name@company.com"
+                                />
+                                {errors.email && (
+                                    <p className="text-red-500 text-xs">
+                                        {errors.email.message}
+                                    </p>
+                                )}
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="password"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    {...register("password", {
+                                        required: "Password is required",
+                                    })}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="••••••••"
+                                />
+                                {errors.password && (
+                                    <p className="text-red-500 text-xs">
+                                        {errors.password.message}
+                                    </p>
+                                )}
+                            </div>
+                            {errorMessage && (
+                                <p className="text-red-500 text-xs">
+                                    {errorMessage}
+                                </p>
+                            )}
+                            <button
+                                type="submit"
+                                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            >
+                                Login
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Login;
