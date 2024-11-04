a5aca62ceadf703c7219c845f6db4572780391e9
phuongvo1110 <vptphuong21@clc.fitus.edu.vn>
2024-11-04 22:59:55 +0700

    update file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Register from './components/Register/Register';
-import Login from './components/Login/Login';
-import Dashboard from './components/Dashboard/Dashboard';
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import Dashboard from "./components/Dashboard/Dashboard";
 function App() {
     return (
-        <Router basename='/Register-App'>
-            <Routes>
-                <Route path='/register' element={<Register />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/' element={<Dashboard/>}/>
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Dashboard />} />
+        </Routes>
     );
 }
 
